427f46453ac0864203168f8ed83002dba6b9c966
Paul Cruse III <paulcruse3@gmail.com>
2024-06-12 14:19:25 -0500

    need to make some functions private

--- chilo/resolver/scanner.py
+++ chilo/resolver/scanner.py
@@ -20,36 +20,36 @@ class ResolverScanner:
         file_path, import_path = self.__get_file_and_import_path(request.path)
         return self.importer.import_module_from_file(file_path, import_path)
 
-    def get_import_path(self, relative_file_path):
-        return relative_file_path.replace(self.importer.file_separator, '.').replace('.py', '')
-
-    def get_request_path_as_list(self, request_path):
-        base_path = request_path.replace(self.base_path, '').replace('-', '_')
-        clean_base = self.importer.clean_path(base_path)
-        return clean_base.split('/')
-
-    def determine_which_file_leaf(self, file_tree, file_branch):
-        if file_tree.get(file_branch) and file_tree[file_branch] != '*':
-            self.file_tree_climbed = True
-            return file_tree[file_branch]
-        self.file_tree_climbed = False
-        return file_tree
-
-    def append_import_path(self, path_part):
-        if self.file_tree_climbed:
-            self.import_path.append(path_part)
-
     def reset(self):
         self.has_dynamic_route = False
         self.dynamic_parts = {}
         self.file_tree_climbed = True
         self.import_path = []
 
+    def __get_import_path(self, relative_file_path):
+        return relative_file_path.replace(self.importer.file_separator, '.').replace('.py', '')
+
+    def __get_request_path_as_list(self, request_path):
+        base_path = request_path.replace(self.base_path, '').replace('-', '_')
+        clean_base = self.importer.clean_path(base_path)
+        return clean_base.split('/')
+
+    def __determine_which_file_leaf(self, file_tree, file_branch):
+        if file_tree.get(file_branch) and file_tree[file_branch] != '*':
+            self.file_tree_climbed = True
+            return file_tree[file_branch]
+        self.file_tree_climbed = False
+        return file_tree
+
+    def __append_import_path(self, path_part):
+        if self.file_tree_climbed:
+            self.import_path.append(path_part)
+
     def __get_file_and_import_path(self, request_path):
-        split_path = self.get_request_path_as_list(request_path)
+        split_path = self.__get_request_path_as_list(request_path)
         route_path = self.__get_relative_path(split_path)
         file_path = self.__handler_pattern.split(f'{self.importer.file_separator}*')[0] + self.importer.file_separator + route_path
-        import_path = self.get_import_path(file_path)
+        import_path = self.__get_import_path(file_path)
         return file_path, import_path
 
     def __get_relative_path(self, split_path):
@@ -78,26 +78,26 @@ class ResolverScanner:
                 raise ApiException(code=404, message='route not found')
 
     def __handle_directory_path_part(self, possible_directory, split_path, split_index, file_tree, file_pattern):
-        self.append_import_path(possible_directory)
+        self.__append_import_path(possible_directory)
         if split_index+1 < len(split_path):
-            file_leaf = self.determine_which_file_leaf(file_tree, possible_directory)
+            file_leaf = self.__determine_which_file_leaf(file_tree, possible_directory)
             self.__get_import_path_file_tree(split_path, split_index+1, file_leaf, file_pattern)
         else:
             index_file = file_pattern.replace('*', possible_directory) if '*' in file_pattern else '__init__.py'
-            self.append_import_path(index_file)
+            self.__append_import_path(index_file)
 
     def __handle_file_path_part(self, possible_file, split_path, split_index, file_tree, file_pattern):
-        self.append_import_path(possible_file)
-        file_leaf = self.determine_which_file_leaf(file_tree, possible_file)
+        self.__append_import_path(possible_file)
+        file_leaf = self.__determine_which_file_leaf(file_tree, possible_file)
         self.__get_import_path_file_tree(split_path, split_index+1, file_leaf, file_pattern)
 
     def __handle_dynamic_path_part(self, split_path, split_index, file_tree, file_pattern):
         file_part = list(file_tree['__dynamic_files'])[0]
-        self.append_import_path(file_part)
+        self.__append_import_path(file_part)
         if '.py' not in file_part and split_index+1 == len(split_path):
             index_file = file_pattern.replace('*', file_part) if '*' in file_pattern else '__init__.py'
-            self.append_import_path(index_file)
-        file_leaf = self.determine_which_file_leaf(file_tree, file_part)
+            self.__append_import_path(index_file)
+        file_leaf = self.__determine_which_file_leaf(file_tree, file_part)
         self.has_dynamic_route = True
         self.dynamic_parts[split_index] = split_path[split_index]
         self.__get_import_path_file_tree(split_path, split_index+1, file_leaf, file_pattern)
