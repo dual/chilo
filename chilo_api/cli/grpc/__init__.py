f6c70b547f610fba3a8f597c16144024338a89f0
Paul Cruse III <paul.cruse@upstart.com>
2024-09-05 16:06:01 -0500

    adding new endpoint properties

--- chilo_api/cli/grpc/__init__.py
+++ chilo_api/cli/grpc/__init__.py
@@ -8,7 +8,7 @@ from chilo_api.cli.grpc.parser import GRPCParser
 from chilo_api.cli.grpc.scanner import GRPCScanner
 
 
-def run_server(server):
+def __run_server(server):
     gprc_server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     # calculator_pb2_grpc.add_CalculatorServicer_to_server(CalculatorServicer(), server)
     gprc_server.add_insecure_port(f'[::]:{server.port}')  # 50051
@@ -16,17 +16,25 @@ def run_server(server):
     gprc_server.wait_for_termination()
 
 
-def generate_middleware_objects(server, endpoints):
+def __generate_middleware_objects(server, endpoints):
     parser = GRPCParser()
     for endpoint in endpoints:
         result = parser.parse_proto_file(server, endpoint)
         for statement in result.statements:
-            print(statement)
+            class_name = type(statement).__name__
+            if class_name == 'Package':
+                endpoint.package_identifier = statement.identifer[0]
+            if class_name == 'Service':
+                endpoint.service_name = statement.name
+                endpoint.response_func_name = statement.body[0].response_message_type
+                endpoint.request_stream = statement.body[0].request_stream
+                endpoint.response_stream = statement.body[0].response_stream
+        print(endpoint)
             # use data from statement to find identifer, name, request_message_type, response_message_type, and string match function
             # https://stackoverflow.com/questions/15247075/how-can-i-dynamically-create-derived-classes-from-a-base-class
 
 
-def generate_grpc_code(server):
+def __generate_grpc_code(server):
     scanner = GRPCScanner()
     importer = GRPCImporter()
     commander = GRPCCommander()
@@ -39,6 +47,6 @@ def generate_grpc_code(server):
 
 
 def run_grpc_simple(server):
-    endpoints = generate_grpc_code(server)
-    generate_middleware_objects(server, endpoints)
-    # run_server(server)
+    endpoints = __generate_grpc_code(server)
+    __generate_middleware_objects(server, endpoints)
+    # __run_server(server)
--- chilo_api/cli/grpc/endpoint.py
+++ chilo_api/cli/grpc/endpoint.py
@@ -8,6 +8,13 @@ class GRPCEndpoint:
         self.__rpc_func_name = kwargs['rpc_func_name']
         self.__requirements  = kwargs['requirements']
         self.__protobuf = self.__requirements['protobuf']
+        self.__package_identifier = ''
+        self.__pb2_module = None
+        self.__pb2_grpc_module = None
+        self.__service_name = ''
+        self.__response_func_name = ''
+        self.__request_stream = False
+        self.__response_stream = False
 
     @classmethod
     def get_endpoints_from_modules(cls, modules):
@@ -46,10 +53,86 @@ class GRPCEndpoint:
     def protobuf(self):
         return self.__protobuf
 
+    @property
+    def package_identifier(self):
+        return self.__package_identifier
+
+    @package_identifier.setter
+    def package_identifier(self, package_identifier):
+        self.__package_identifier = package_identifier
+
+    @property
+    def pb2_file(self):
+        return f'{self.__package_identifier}_pb2'
+
+    @property
+    def pb2_grpc_file(self):
+        return f'{self.__package_identifier}_pb2_grpc'
+    
+    @property
+    def pb2_module(self):
+        return self.__pb2_module
+
+    @pb2_module.setter
+    def pb2_module(self, pb2_module):
+        self.__pb2_module = pb2_module
+
+    @property
+    def pb2_grpc_module(self):
+        return self.__pb2_grpc_module
+
+    @pb2_grpc_module.setter
+    def pb2_grpc_module(self, pb2_grpc_module):
+        self.__pb2_grpc_module = pb2_grpc_module
+    
+    @property
+    def service_name(self):
+        return self.__service_name
+
+    @service_name.setter
+    def service_name(self, service_name):
+        self.__service_name = service_name
+    
+    @property
+    def servicer_name(self):
+        return f'{self.service_name}Servicer'
+
+    @property
+    def response_func_name(self):
+        return self.__response_func_name
+
+    @response_func_name.setter
+    def response_func_name(self, response_func_name):
+        self.__response_func_name = response_func_name
+    
+    @property
+    def request_stream(self):
+        return self.__request_stream
+
+    @request_stream.setter
+    def request_stream(self, request_stream):
+        self.__request_stream = request_stream
+    
+    @property
+    def response_stream(self):
+        return self.__response_stream
+
+    @response_stream.setter
+    def response_stream(self, response_stream):
+        self.__response_stream = response_stream
+
     def __str__(self):
         return json.dumps({
             'rpc_func_name': self.rpc_func_name,
             'rpc_name': self.rpc_name,
             'requirements': self.requirements,
-            'protobuf': self.protobuf
+            'protobuf': self.protobuf,
+            'package_identifier': self.package_identifier,
+            'pb2_file': self.pb2_file,
+            'pb2_grpc_file': self.pb2_grpc_file,
+            'service_name': self.service_name,
+            'servicer_name': self.servicer_name,
+            'response_func_name': self.response_func_name,
+            'request_stream': self.request_stream,
+            'response_stream': self.response_stream
         })
