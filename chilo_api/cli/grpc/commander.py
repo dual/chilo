417f9e2354d0a8508d2b3745a83bf647ab494c40
Paul Cruse III <paul.cruse@upstart.com>
2024-09-05 14:47:01 -0500

    creating folder if it doesn't exist

--- chilo_api/cli/grpc/commander.py
+++ chilo_api/cli/grpc/commander.py
@@ -1,3 +1,4 @@
+import pathlib
 import subprocess
 
 
@@ -6,8 +7,10 @@ class GRPCCommander:
     def generate_grpc_code(self, endpoints, server):
         for endpoint in endpoints:
             generated = f'{server.protobufs}/generated'
+            path = pathlib.Path(generated)
             file = f'{server.protobufs}/{endpoint.protobuf}'
             command = ['python', '-m', 'grpc_tools.protoc']
             args = f'--proto_path={server.protobufs} --python_out={generated} --pyi_out={generated} --grpc_python_out={generated} {file}'
             command.extend(args.split())
+            path.mkdir(parents=True, exist_ok=True)
             subprocess.run(command)
